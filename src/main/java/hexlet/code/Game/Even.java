6d2672e53f3f30ecab7de1a43714f33a6045f018
AidanKlark <davydov-2@yandex.ru>
2021-08-26 21:28:32 +0300

    made EVEN

--- src/main/java/hexlet/code/Game/Even.java
+++ src/main/java/hexlet/code/Game/Even.java
@@ -8,13 +8,12 @@ public class Even {
 
     static String NAME = Cli.name();
     static String INSTRUCTION = "Answer 'yes' if number even otherwise answer 'no'.";
-    static String right;
     static String playerAnswer;
     static String answer;
     static String YES = "yes";
     static String NO = "no";
     static int n = 3;                                                                       //Количество верных ответов
-    static int count = 0;
+    static int number;
 
     public static void even() {
 
@@ -25,11 +24,14 @@ public class Even {
 
     public static void conclusion() {
 
+        int count = 0;
         boolean allAnswersTrue = true;
 
         do {
             ++count;
 
+            number = genRandom();
+            showQuestion();
             answer = correctAnswer();
             playerAnswer = task();
 
@@ -37,31 +39,41 @@ public class Even {
                 System.out.println("Correct!");
             }
             else {
-                System.out.printf("%s is wrong answer ;(. Correct answer was %s.", playerAnswer, answer);
+                System.out.printf("%s is wrong answer ;(. Correct answer was %s.\n Let's try again, %s!", playerAnswer, answer, NAME);
                 allAnswersTrue = false;
             }
         } while (count < n) ;
 
         if (allAnswersTrue) {
-            System.out.printf("Congratulations, %s !", NAME);
+            System.out.printf("Congratulations, %s!", NAME);
         }
     }
 
     public static String correctAnswer() {
 
-        answer = genRandom() ? YES : NO;
+        answer = isEven() ? YES : NO;
+        return answer;
     }
 
     static int genRandom() {
 
         Random random = new Random();
-
-        return Integer.valueOf(random.nextInt(100 + 1));
+        return random.nextInt(100 + 1);
     }
 
     public static String task() {
 
         System.out.print("Your answer: ");
         return new Scanner(System.in).nextLine();
+    }
+
+    public static boolean isEven() {
+
+        return number % 2 == 0;
+    }
+
+    public static void showQuestion() {
+
+        System.out.printf("Question: %s\n", number);
     }
 }
